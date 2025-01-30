26f348c8f87a448aab7eebfea928e5e26d7fab11
ogsavimaja <leevi.alanappa@gmail.com>
2025-01-30 23:34:06 +0200

    Add ability to create new announcements
    - Add ability to create new announcements
    - Add table Announcements to schema.sql
    - Add new_announcement.html for announcement form
    - Make code easier to read

--- App.py
+++ App.py
@@ -16,26 +16,59 @@ app.secret_key = config.secret_key
 def index():
     return render_template("index.html")
 
+@app.route("/new_announcement", methods=["GET", "POST"])
+def new_announcement():
+    if not session.get("username"):
+        return redirect("/login")
+    if request.method == "POST":
+        title = request.form["title"]
+        description = request.form["description"]
+        intented_price = request.form["intented_price"]
+        age_restriction = request.form["age_restriction"]
+
+        # Validate user input
+        if not title or not description:
+            return "ERROR: All fields marked with * are required"
+        if intented_price:
+            if not intented_price.isdigit():
+                return "ERROR: Price must be a number"
+        if age_restriction:
+            if not age_restriction.isdigit():
+                return "ERROR: Age restriction must be a number"
+
+        # Insert announcement into database
+        sql_query = """INSERT INTO Announcements (user_id, title, about, intented_price, intented_age_restriction)
+                       VALUES (?, ?, ?, ?, ?)"""
+        db.execute(sql_query, [session["user_id"], title, description, intented_price, age_restriction,])
+        return redirect("/")
+
+    return render_template("new_announcement.html")
+
 # Render login page
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    # Check if user is already logged in
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
         if not username or not password:
             return "ERROR: All fields are required"
-        
+
         # Check if user exists
-        user_data = db.query("SELECT * FROM Users WHERE username = ?", [username])
+        sql_query = """SELECT *
+                       FROM Users
+                       WHERE username = ?"""
+        user_data = db.query(sql_query, [username])
         if not user_data:
             return "ERROR: Invalid Credentials"
-        
+
         # Check if password is correct
         user_data = user_data[0]
         if check_password_hash(user_data["hashed_password"], (password + user_data["salt"])):
             session["username"] = user_data["username"]
+            session["user_id"] = user_data["id"]
             return redirect("/")
-        
+
         return "ERROR: Invalid Credentials"
 
     return render_template("login.html")
@@ -44,6 +77,7 @@ def login():
 @app.route("/logout")
 def logout():
     del session["username"]
+    del session["user_id"]
     return redirect("/")
 
 # Render register page
@@ -72,14 +106,19 @@ def create():
     if email:
         if not ("@" and "." in email):
             return "Invalid email"
-    
+
+
     # Check if username already exists
     try:
-        db.execute("INSERT INTO Users (username, salt, hashed_password, email) VALUES (?, ?, ?, ?)", [username, salt, generate_password_hash(password + salt), email])
+        sql_query = """INSERT INTO Users (username, salt, hashed_password, email)
+                       VALUES (?, ?, ?, ?)"""
+        db.execute(sql_query, [username, salt, generate_password_hash(password + salt), email])
     except sqlite3.IntegrityError:
         return "ERROR: Username already exists"
-    
+
     return "Account created"
+
+
 
 
 # Allows the app to run in IDE terminal in debug mode
--- schema.sql
+++ schema.sql
@@ -5,3 +5,14 @@ CREATE TABLE Users (
     hashed_password TEXT NOT NULL,
     email TEXT
 );
+
+CREATE TABLE Announcements (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES Users(id),
+    title TEXT NOT NULL,
+    about TEXT NOT NULL,
+    intented_price INTEGER,
+    intented_age_restriction INTEGER,
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+    updated_at TEXT
+);
